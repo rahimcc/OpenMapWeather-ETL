09245ab9dd1045c2d1915e0a2777cb8ba8a8e424
Rahim Sharifov <sharifov.rahimcc@gmail.com>
2026-08-20 12:24:26 +0400

    Initial streamlit app

---
+++ streamlit_app/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title = "Baku Weather Dashboard", layout="wide")
+st.title("🌤️ Baku Weather Dashboard")
+st.write("Hello — Streamlit is running.")
+
+
